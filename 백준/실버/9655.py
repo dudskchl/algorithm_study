b24c8255ecb5de3c2184e0e6ff1e38e33bd04b6d
LuoYing <48114421+dudskchl@users.noreply.github.com>
2023-06-12 23:08:50 +0900

    Add files via upload

---
+++ 백준/실버/9655.py
@@ -0,0 +1,54 @@
+
+
+N=int(input())
+
+print('CY') if N%2==0 else print('SK')
+
+
+#N=1
+#turn 1: 상근 1개
+#상근 win
+
+#N=2
+#turn 1:상근 1개
+#turn 2:창영 1개
+#창영 win
+
+#N=3
+#turn 1:상근 3개
+#상근 win
+
+#N=4
+#turn 1:상근 1개
+#turn 2:창영 3개
+#창영 win
+
+#N=5
+#turn 1:상근 3개
+#turn 2:창영 1개
+#turn 3:상근 1개
+#상근 win
+
+#N=6
+#turn 1:상근 1개
+#turn 2:창영 3개
+#turn 3:상근 1개
+#turn 4:창영 1개
+#창영 win
+
+#N=7
+#turn 1:상근 3개
+#turn 2:창영 1개
+#turn 3:상근 3개
+#상근 win
+
+#N=8
+#turn 1:상근 3개
+#turn 2:창영 3개
+#turn 3:상근 1개
+#turn 4:창영 1개
+#창영 win
+
+
+
+
